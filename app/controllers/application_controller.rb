5955cc0f9e642be221d3e7a6e09274fd54e81f94
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-28 18:08:37 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,44 +11,41 @@ class ApplicationController < Sinatra::Base
     redirect to "/articles"
   end
 
-  get '/articles/new' do 
+  get '/articles/new' do #OK
+    @article = Article.new
     erb :new
   end 
 
-  get '/articles' do 
-    @articles = Article.all 
+  post '/articles' do #OK
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end 
+
+  get '/articles' do #OK
+    @articles = Article.all
     erb :index
   end 
 
-  get '/articles/:id' do
-    @article = Article.find_by(id: params[:id])
+  get '/articles/:id' do #OK
+    @article = Article.find(params[:id])
     erb :show
   end 
 
-  get '/articles/:id/edit' do 
-    @article = Article.find_by_id(params[:id])
+  get '/articles/:id/edit' do #OK
+    @article = Article.find(params[:id])
     erb :edit
   end 
 
   patch '/articles/:id' do 
-    @article = Article.find_by_id(params[:id])
-    @article.name = params[:name]
-    @article.content = params[:content]
-    @article.save
-    erb :show
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
   end 
 
-  post '/articles' do
-    @article = Article.create(name: params[:name], content: params[:content])
-    redirect to '/articles'
+  delete '/articles/:id' do 
+    Article.destroy(params[:id])
+    redirect to "/articles"
   end 
 
-
-  delete '/articles/:id/delete' do
-    @article = Article.find_by_id(params[:id])
-    @thing = @article.name
-    @article.delete
-    erb :delete
-  end 
 
 end
